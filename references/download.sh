9cfd701225eb1650f175732d198431ae0a4d0b4f
Anime4000 <ilham92_sakura@yahoo.com>
2015-09-19 05:21:35 +0800

    fix wrong folder

--- references/download.sh
+++ references/download.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd "$(dirname $(readlink -f $0))"
-wget --no-check-certificate https://github.com/Anime4000/IFME/releases/download/v5.0-beta.8/INIFileParser.dll -O "references/INIFileParser.dll"
-wget --no-check-certificate https://github.com/x265/MediaInfoDotNet/releases/download/v0.7.8/MediaInfoDotNet.dll -O "references/IMediaInfoDotNet.dll"
+wget --no-check-certificate https://github.com/Anime4000/IFME/releases/download/v5.0-beta.8/INIFileParser.dll -O "INIFileParser.dll"
+wget --no-check-certificate https://github.com/x265/MediaInfoDotNet/releases/download/v0.7.8/MediaInfoDotNet.dll -O "MediaInfoDotNet.dll"
 echo "Done!!!"
 sleep 3
