8a1b5e11ec42047c745d453c86721b263728119f
Anime4000 <ilham92_sakura@yahoo.com>
2015-09-21 07:53:49 +0800

    sh script to run

--- compile.sh
+++ compile.sh
@@ -86,7 +86,7 @@ mkbundle --deps --static -o ifme-bin ifme.exe INIFileParser.dll MediaInfoDotNet.
 gcc "../sources/ifme-gnome.c" -o "ifme-gnome"
 gcc "../sources/ifme-xterm.c" -o "ifme-xterm"
 
-cp -a "../source/ifme.sh" "$BUILDDIR/ifme"
+cp "../source/ifme.sh" "ifme"
 chmod +x "$BUILDDIR/ifme"
 
 echo "Remove bytecode"
