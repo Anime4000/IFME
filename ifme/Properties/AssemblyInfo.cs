887dc62ceffac61002bfae2a4b9f552b9c3ca876
Anime4000 <ilham92_sakura@yahoo.com>
2015-11-11 06:37:49 +0800

    fix version

--- ifme/Properties/AssemblyInfo.cs
+++ ifme/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Resources;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("5.1.*")]
-[assembly: AssemblyFileVersion("5.1.3")]
+[assembly: AssemblyFileVersion("5.1.4")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
