fbdc3f72332bef2a6677ba915402a5bc1c3f31d2
Anime4000 <ilham92_sakura@yahoo.com>
2015-12-03 04:10:49 +0800

    mingw autobuilds

--- sources/compile-x265-win64/hg_x265.sh
+++ sources/compile-x265-win64/hg_x265.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# This script intend for automatic builds for IFME
+# Enjoy!
+
 rm -rf x265
 hg clone https://bitbucket.org/multicoreware/x265 x265
 
@@ -8,4 +11,39 @@ mkdir icl64
 
 # MSYS
 cd msys
-wget 
+wget https://github.com/Anime4000/IFME/raw/master/sources/compile-x265-win64/x265-msys.sh
+sh x265-msys.sh
+
+cd ..
+
+# VS12
+cd vc12-x86_64
+wget https://github.com/Anime4000/IFME/raw/master/sources/compile-x265-win64/x265-vs12.cmd
+cmd /c x265-vs12.cmd
+
+cd ..
+
+# ICL64
+cd icl64
+wget https://github.com/Anime4000/IFME/raw/master/sources/compile-x265-win64/x265-icl64.cmd
+cmd /c x265-icl64.cmd
+
+cd ..
+
+# Capture
+mkdir ifme
+mkdir ifme/x265gcc
+mkdir ifme/x265msvc
+mkdir ifme/x265icc
+
+cp msys/x265-08.exe ifme/x265gcc/
+cp msys/x265-10.exe ifme/x265gcc/
+cp msys/x265-12.exe ifme/x265gcc/
+
+cp vc12-x86_64/x265-08.exe ifme/x265msvc/
+cp vc12-x86_64/x265-10.exe ifme/x265msvc/
+cp vc12-x86_64/x265-12.exe ifme/x265msvc/
+
+cp icl64/x265-08.exe ifme/x265icc/
+cp icl64/x265-10.exe ifme/x265icc/
+cp icl64/x265-12.exe ifme/x265icc/
