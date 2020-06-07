482a166d3ede502510d48b2428929cedb9b15fe6
Anime4000 <ilham92_sakura@yahoo.com>
2020-06-08 02:45:05 +0800

    culture for all threads

--- IFME/Program.cs
+++ IFME/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
+using System.Globalization;
 
 namespace IFME
 {
@@ -17,9 +14,11 @@ namespace IFME
         [STAThread]
         static void Main()
         {
-			SetDefaultCulture(new CultureInfo("en-us"));
+            var culture = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
 
-			if (Properties.Settings.Default.UpgradeRequired)
+            if (Properties.Settings.Default.UpgradeRequired)
             {
                 Properties.Settings.Default.Upgrade();
                 Properties.Settings.Default.UpgradeRequired = false;
@@ -31,46 +30,5 @@ namespace IFME
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmMain());
         }
-
-		/// <summary>
-		/// Change program localisation, allow to cast properly
-		/// </summary>
-		/// <param name="culture">Culture Id, example: en-us</param>
-		static void SetDefaultCulture(CultureInfo culture)
-		{
-			Type type = typeof(CultureInfo);
-
-			try
-			{
-				type.InvokeMember("s_userDefaultCulture",
-					BindingFlags.SetField | BindingFlags.NonPublic | BindingFlags.Static,
-					null,
-					culture,
-					new object[] { culture });
-
-				type.InvokeMember("s_userDefaultUICulture",
-					BindingFlags.SetField | BindingFlags.NonPublic | BindingFlags.Static,
-					null,
-					culture,
-					new object[] { culture });
-			}
-			catch { }
-
-			try
-			{
-				type.InvokeMember("m_userDefaultCulture",
-					BindingFlags.SetField | BindingFlags.NonPublic | BindingFlags.Static,
-					null,
-					culture,
-					new object[] { culture });
-
-				type.InvokeMember("m_userDefaultUICulture",
-					BindingFlags.SetField | BindingFlags.NonPublic | BindingFlags.Static,
-					null,
-					culture,
-					new object[] { culture });
-			}
-			catch { }
-		}
 	}
 }
