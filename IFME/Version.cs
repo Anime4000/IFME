d615654b96934900e57dbb6edb61fd301e3dac34
Anime4000 <ilham92_sakura@yahoo.com>
2024-02-28 00:50:49 +0800

    forgot to fix this haha

--- IFME/Version.cs
+++ IFME/Version.cs
@@ -12,7 +12,7 @@ namespace IFME
         public static string Title { get { return "Internet Friendly Media Encoder"; } }
         public static string Name { get { return typeof(Program).Assembly.GetName().Name; } }
         public static string CodeName { get { return Application.ProductName; } }
-        public static string Release { get { return $"{version[0]}.{version[1]} beta {version[2]}"; } }
+        public static string Release { get { return $"{version[0]}.{version[1]}{(version.Length > 2 ? $" beta {version[2]}" : "" )}"; } }
         public static string Contrib { get { return "Nemu, Jel42, Robin Lawrie, Ron Mitschke, Project Angel"; } }
         public static string TradeMark { get { return FileVersionInfo.GetVersionInfo(Assembly.GetEntryAssembly().Location).LegalTrademarks; } }
         public static string March { get { return "znver2"; } }
