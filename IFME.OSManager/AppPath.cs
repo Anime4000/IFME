c3206f0248817beab2ae82a03cb478a71386c003
Anime4000 <hitoha4000@outlook.com>
2025-05-19 23:40:33 +0800

    make own path logic

---
+++ IFME.OSManager/AppPath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+public static class AppPath 
+{
+    /// <summary>
+    /// Builds a path from the given parts, ensuring that it is a full path.
+    /// </summary>
+    /// <param name="parts"></param>
+    /// <returns></returns>
+    public static string Combine(params string[] parts)
+    {
+        return Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, Path.Combine(parts)));
+    }
+}
